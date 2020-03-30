0905b9e90574660dec3786e7075fabd061ab138a
Viaceslav-Kodesh <54391135+Viaceslav-Kodesh@users.noreply.github.com>
2020-03-31 02:11:39 +0300

    Update action.py

--- action.py
+++ action.py
@@ -11,7 +11,7 @@ import os
 
 def get_nova_credentials_v2():
     d = {}
-    d['version'] = os.environ['OS_NOVA_VERSION']
+    d['version'] = '2.0'
     d['username'] = os.environ['OS_USERNAME']
     d['api_key'] = os.environ['OS_PASSWORD']
     d['auth_url'] = os.environ['OS_AUTH_URL']
